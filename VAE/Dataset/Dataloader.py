1dccf486c021e0c793d8ef5dc42aec4867496a38
SrBalto <nestor.alvarez@ug.uchile.cl>
2023-07-15 18:41:58 -0400

    improvement on dataloader

--- VAE/Dataset/Dataloader.py
+++ VAE/Dataset/Dataloader.py
@@ -3,8 +3,18 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset
 from torchvision.io import read_image
+from torchvision import transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+import torchvision.transforms.functional as F
+
+transform = transforms.Compose([
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+
 class CustomImageDataset(Dataset):
     def __init__(self, img_dir, transform=None, target_transform=None):
         self.img_dir = img_dir
@@ -22,15 +32,23 @@ class CustomImageDataset(Dataset):
             image = self.transform(image)
         return image
 
-batch_size=9
-training_data=CustomImageDataset("celeb_A/img_align_celeba")
-train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
-train_images = next(iter(train_dataloader))
-batch_array = np.transpose(train_images, (0, 2, 3, 1))
-fig, axes = plt.subplots(3, 3)
-for i, ax in enumerate(axes.flat):
-    ax.imshow(batch_array[i])
-    ax.axis('off')
-#TODO: show on the plot info like img size
-plt.tight_layout()
-plt.show()
+if __name__=='__main__':
+    # now use it as the replacement of transforms.Pad class
+    transform_view=transforms.Compose([
+        transforms.CenterCrop(224),
+        #transforms.ToTensor(),
+        #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+    batch_size=9
+    training_data=CustomImageDataset("celeb_A/img_align_celeba",transform=transform_view)
+    train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+    train_images = next(iter(train_dataloader))
+    batch_array = np.transpose(train_images, (0, 2, 3, 1))
+    fig, axes = plt.subplots(3, 3)
+    for i, ax in enumerate(axes.flat):
+        print(batch_array[i].shape)
+        ax.imshow(batch_array[i])
+        ax.axis('off')
+    #TODO: show on the plot info like img size
+    plt.tight_layout()
+    plt.show()
